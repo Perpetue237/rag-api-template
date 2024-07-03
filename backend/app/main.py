3c8aa810c4d00794d5b0382a4850a3a0ce4b7a4b
Kuete Tiayo, Perpetue <perpetue.kuete@hessian.ai>
2024-07-03 10:46:08 +0000

    docs(documentation): in code

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 import os, json, requests
 import dotenv, asyncio
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline, GenerationConfig
@@ -17,28 +18,31 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain_community.llms import HuggingFacePipeline
 
-
-
 from fastapi import FastAPI, HTTPException, Query, UploadFile, File
 from pydantic import BaseModel
 
 from typing import AsyncGenerator
 
 import logging
+
+# Set up logging
 logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-
-
+# Initialize FastAPI app
 app = FastAPI()
+
+# Define paths for uploads, models, and tokenizers
 app.file_path = None
 app.UPLOAD_DIRECTORY = "/app/rag-uploads"
 app.MODEL_DIRECTORY = "/app/models"
 app.TOKENIZER_DIRECTORY = "/app/tokenizers"
 
+# Allow all origins for CORS
 origins = ["*"]
 
+# Add CORS middleware to the app
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -47,100 +51,126 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Create upload directory if it doesn't exist
 if not os.path.exists(app.UPLOAD_DIRECTORY):
     os.makedirs(app.UPLOAD_DIRECTORY)
 
+# Define a Pydantic model for query requests
 class QueryRequest(BaseModel):
     query: str
     context: str = None
     
+# Function to format documents
 def format_docs(docs):
     return " ".join([doc.page_content.replace('\n', ' ') for doc in docs])
 
-    
+# Define model and tokenizer paths
 model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 model_path = os.path.join(app.MODEL_DIRECTORY, model_name)
 tokenizer_path = os.path.join(app.TOKENIZER_DIRECTORY, model_name)
 
-# Load the model and tokenizer from the local directories
+# Load tokenizer
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
+# Set up quantization configuration
 bnb_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16)
-model = AutoModelForCausalLM.from_pretrained(model_path,
-                                                device_map="auto",
-                                                quantization_config=bnb_config)
+
+# Load model with quantization
+model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", quantization_config=bnb_config)
+
+# Set up embeddings
 embeder = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large", cache_folder="/app/cache")
 
+# Set pad token for the tokenizer
 tokenizer.pad_token = tokenizer.eos_token
-generation_config = GenerationConfig(
-            max_new_tokens=1000,
-            do_sample=True,
-            bos_token_id=tokenizer.bos_token_id,
-            eos_token_id=tokenizer.eos_token_id,
-            pad_token_id=tokenizer.eos_token_id)
 
+# Define generation configuration
+generation_config = GenerationConfig(
+    max_new_tokens=1000,
+    do_sample=True,
+    bos_token_id=tokenizer.bos_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+    pad_token_id=tokenizer.eos_token_id
+)
+
+# Set up text generation pipeline
 pipe = pipeline(
-            "text-generation", 
-            model=model,
-            tokenizer=tokenizer,
-            max_new_tokens=1000
-        )
+    "text-generation", 
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=1000
+)
+
+# Set up HuggingFace pipeline
 hf_pipe = HuggingFacePipeline(pipeline=pipe, model_kwargs={"generation_config": generation_config})
 
-
-
-   
+# Endpoint to upload a file
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    logger.info(f"UPLAODING FILE WITH NAME {file.filename}")
+    logger.info(f"UPLOADING FILE WITH NAME {file.filename}")
     file_location = os.path.join(app.UPLOAD_DIRECTORY, file.filename)
     app.file_path = file_location
     with open(file_location, "wb") as f:
         f.write(file.file.read())
     return {"filePath": file_location}
 
+# Endpoint to retrieve answers from the document at a given path
 @app.get("/retrieve_from_path")
 async def retrieve_from_path(file_path: str = Query(...), question: str = Query(...)):
     
+    # Generator to provide asynchronous response
     async def answer_generator() -> AsyncGenerator[str, None]:
         logger.info(f"RECEIVED REQUEST WITH FILE PATH {file_path} AND QUESTION {question}")
+        
+        # Load documents from the file
         loader = PyPDFLoader(file_path)
         documents = loader.load()
+        
+        # Split documents into chunks
         text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
         chunks = text_splitter.split_documents(documents)
-        vectorstore = FAISS.from_documents(
-                                            documents = chunks,
-                                            embedding = embeder
-                                            )
+        
+        # Create a FAISS vector store from the document chunks
+        vectorstore = FAISS.from_documents(documents=chunks, embedding=embeder)
         retriever = vectorstore.as_retriever()
 
+        # Define prompt template
         prompt = ChatPromptTemplate.from_template(
-        template="Given the context: {context}, and the question: {question}, provide a detailed answer."
+            template="Given the context: {context}, and the question: {question}, provide a detailed answer."
         )
+        
+        # Define RAG chain from documents
         rag_chain_from_docs = (
-                                RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
-                                | prompt
-                                | hf_pipe
-                                | StrOutputParser()
-                            )
+            RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
+            | prompt
+            | hf_pipe
+            | StrOutputParser()
+        )
 
+        # Define the main RAG chain
         rag_chain = RunnableParallel(
-                                {"context":retriever, "question": RunnablePassthrough()}
-                            ).assign(answer=rag_chain_from_docs)
+            {"context": retriever, "question": RunnablePassthrough()}
+        ).assign(answer=rag_chain_from_docs)
+        
+        # Invoke the RAG chain with the question
         result = rag_chain.invoke(question)
 
+        # Extract relevant context
         relevant_context = result.get('context', [])
         formated_context = [element.page_content.replace('\n', ' ') for element in relevant_context]
 
-        yield json.dumps({"context": formated_context})+ '\n'
+        # Yield formatted context
+        yield json.dumps({"context": formated_context}) + '\n'
+        
+        # Yield answer tokens one by one with a delay
         for token in result["answer"].split():
             yield json.dumps({"answer": token}) + '\n'
             await asyncio.sleep(0.1)
 
+    # Return streaming response
     return StreamingResponse(answer_generator(), media_type="application/json")
-    
 
- 
+# Main entry point for running the app
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
